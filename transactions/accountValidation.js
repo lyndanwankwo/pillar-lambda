0d8fc294ae803a8a2a3e99a6ae755c018e107b9c
lyndanwankwo <83649911+lyndanwankwo@users.noreply.github.com>
2021-10-25 11:06:08 -0400

    restructure

--- transactions/accountValidation.js
+++
@@ -1,8 +0,0 @@
-/*
-Validate the schema coming into the API body
-*/
-
-const { AccountContext } = require("./accountContext");
-
-//Validate the info coming in in each type of call
-//Create another aux data push to keep track of historical data
